76ded7f8599e3df46470498147aed90223f84f70
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-06-27 12:42:59 -0300

    m

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Cursos
 
-
-1. [Curso de Rust: a linguagem de programação performática e segura](https://cursos.alura.com.br/course/rust-linguagem-programacao-performatica-segura): Inicio - 23/06/2023 : Fim --/--/----
+1. [Curso de Rust: a linguagem de programação performática e segura](https://cursos.alura.com.br/course/rust-linguagem-programacao-performatica-segura): Inicio - 23/06/2023 : Fim 27/06/2023
+2. [urso de Rust: aprenda mais sobre tipos](https://cursos.alura.com.br/course/rust-aprenda-sobre-tipos): Inicio - 27/06/2023 : Fim --/--/----
--- curso1/aula04/main.rs
+++ curso1/aula04/main.rs
@@ -1,14 +1,43 @@
 fn main() {
-    ownership()
+    ownership();
+
+    pattern_matching();
+
+    erros();
+
 }
 
 fn ownership() {
-    let uma_string = String::from("Vinicius");
-    rouba(uma_string);
+    let mut uma_string = String::from("Vinicius");
+    rouba(&mut uma_string);
 
     println!("{}", uma_string)
 }
 
-fn rouba(string: String) {
+fn rouba(string: &mut String) {
+    string.push_str(" Dias");
     println!("{}", string);
+}
+
+fn pattern_matching() {
+    for x in 1..=20 {
+        println!("{}: {}", x, match x{
+            1 => "Pouco",
+            2 | 3  => "Um pouquinho",
+            4..=10 => "Um bocado",
+            _ if x % 2 == 0 => "Uma boa quantidade",
+            _ => "Muito"
+        });
+    }
+}
+
+fn erros() {
+    match resultado() {
+        Ok(s) => println!("String de sucesso = {}", s),
+        Err(numero) => println!("Codigo de erro = {}", numero)
+    };
+}
+
+fn resultado() -> Result<String, u8> {
+    Ok(String::from("Tudo deu certo"))
 }
