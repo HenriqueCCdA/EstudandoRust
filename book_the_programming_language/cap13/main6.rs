1fdac50e90a1c38329166bca72ca059d40ccd645
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-04 22:36:33 -0300

    m

--- book_the_programming_language/cap13/main6.rs
+++ book_the_programming_language/cap13/main6.rs
@@ -14,6 +14,7 @@ fn main() {
     let mut sort_operations = vec![];
     let value = String::from("by key called");
 
+
     list.sort_by_key(|r| {
         sort_operations.push(value);
         r.width
