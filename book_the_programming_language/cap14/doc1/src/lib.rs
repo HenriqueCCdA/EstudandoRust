29769efa34b2f13f303a5f7a77c63fb8aa59763f
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-05 18:14:52 -0300

    m

---
+++ book_the_programming_language/cap14/doc1/src/lib.rs
@@ -0,0 +1,19 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performainf certain
+//! calculations more convenient.
+
+
+/// Adds one to the number given
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5
+/// let answer = my_crate::addd_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn and_one(x: i32) -> i32 {
+    x + 1
+}
---
+++ book_the_programming_language/cap14/doc2/src/main.rs
@@ -0,0 +1,7 @@
+use art::{mix, PrimaryColor};
+
+fn main() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;;
+    mix(red, yellow);
+}
