88446b78ea2d2f2d2ad2e9c50c8c3cfc7f945dea
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-08-29 00:42:39 -0300

    Cap 19

---
+++ rust_by_example/cap19/ex19_7_d.rs
@@ -0,0 +1,17 @@
+use std::time::Duration;
+use std::sync::Arc;
+use std::thread;
+
+fn main() {
+    let apple = Arc::new("the same apple");
+
+    for _ in 0..10 {
+        let apple = Arc::clone(&apple);
+
+        thread::spawn(move || {
+            println!("{:?}", apple);
+        });
+    }
+
+    thread::sleep(Duration::from_secs(1));
+}
